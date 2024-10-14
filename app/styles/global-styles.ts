c5d2ebde4b6b14d4ace80c99557c2eb69cdbca13
pawelek <k.pawelek@vp.pl>
2024-10-14 14:05:37 +0200

    Update global-styles.ts

--- app/styles/global-styles.ts
+++ app/styles/global-styles.ts
@@ -2,9 +2,12 @@ import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
   body {
+    font-family: 'Geist Sans', sans-serif;
+  }
+
+  * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: 'Geist Sans', sans-serif;
   }
 `;
