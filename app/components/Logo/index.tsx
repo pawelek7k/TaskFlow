e0a1580a076ba60f6d84b101c89cb696454f6e1a
pawelek <k.pawelek@vp.pl>
2024-10-14 20:31:44 +0200

    add logo

--- app/components/Logo/index.tsx
+++ app/components/Logo/index.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
+import styles from "./logo.module.css";
 
 export const Logo = () => {
   return (
     <div>
-      <Link href="/">
+      <Link href="/" className={styles.logo}>
         <h1>
           Task<span>Flow</span>
         </h1>
