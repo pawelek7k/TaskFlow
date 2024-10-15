786cf8d4c047373d1a18d835f8e05b6b86e07e38
pawelek <k.pawelek@vp.pl>
2024-10-15 15:52:33 +0200

    add styles  fixes and performance fixes

--- app/components/CardsList/index.tsx
+++ app/components/CardsList/index.tsx
@@ -1,13 +1,16 @@
 import { cardItems } from "@/app/arrays/IconsList";
+import Link from "next/link";
 import styles from "./cards-list.module.css";
 
 export const CardsList = () => {
   return (
     <ul className={styles.list}>
-      {cardItems.map((item, index) => (
-        <li key={index} className={styles.item}>
-          <div>{item.icon}</div>
-          <span>{item.label}</span>
+      {cardItems.map((item) => (
+        <li key={item.label} className={styles.item}>
+          <Link href={`/${item.label.toLowerCase()}`} aria-label={item.label}>
+            <span>{item.icon}</span>
+            <span>{item.label}</span>
+          </Link>
         </li>
       ))}
     </ul>
