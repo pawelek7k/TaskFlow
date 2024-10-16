93ceb22b2ac8a40f62d1dbfeae5bdfbe5dc34bbf
pawelek <k.pawelek@vp.pl>
2024-10-16 16:11:08 +0200

    Update index.tsx

--- app/components/DashboardContainer/index.tsx
+++ app/components/DashboardContainer/index.tsx
@@ -1,29 +1,23 @@
 import styles from "./container.module.css";
 
+const tasks = [
+  { status: "To do" },
+  { status: "InWork" },
+  { status: "Review" },
+  { status: "Done" },
+];
+
 export const DashboardContainer = () => {
   return (
     <div className={styles.dashboardContainer}>
       <ul className={styles.tasksList}>
-        <li>
-          <div className={styles.circle}></div>
-          <div className={styles.defaultColor}></div>
-          <div>To do</div>
-        </li>
-        <li>
-          <div className={styles.circle}></div>
-          <div className={styles.defaultColor}></div>
-          <div>InWork</div>
-        </li>
-        <li>
-          <div className={styles.circle}></div>
-          <div className={styles.defaultColor}></div>
-          <div>Review</div>
-        </li>
-        <li>
-          <div className={styles.circle}></div>
-          <div className={styles.defaultColor}></div>
-          <div>Done</div>
-        </li>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <div className={styles.circle}></div>
+            <div className={styles.defaultColor}></div>
+            <div>{task.status}</div>
+          </li>
+        ))}
       </ul>
     </div>
   );
