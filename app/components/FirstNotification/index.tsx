90bb60f1663ae7edbd88d8cc42318ac2c76c7798
pawelek <k.pawelek@vp.pl>
2024-10-16 18:00:04 +0200

    Create index.tsx

---
+++ app/components/FirstNotification/index.tsx
@@ -0,0 +1,11 @@
+export const FirstNotificationForUser = () => {
+  return (
+    <div>
+      <h3>Hey!</h3>
+      <p>
+        You can change the category color for better recognition in our
+        application!
+      </p>
+    </div>
+  );
+};
