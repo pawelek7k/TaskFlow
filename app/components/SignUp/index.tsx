6389cf4f9e0cb4b560dcca20e98ced76b2576538
pawelek <k.pawelek@vp.pl>
2024-10-20 14:51:50 +0200

    add sign up styles

--- app/components/SignUp/index.tsx
+++ app/components/SignUp/index.tsx
@@ -1,13 +1,24 @@
+import Image from "next/image";
+import { SecondaryButton } from "../Buttons/SecondaryButton";
+import styles from "./styles.module.css";
+
 export const SignUp: React.FC = () => {
   return (
     <div>
-      <form action="#">
-        <h1>Create Account</h1>
-        <span>or use your email for registration</span>
+      <form action="#" className={styles.form}>
+        <div>
+          <h2>Create Account</h2>
+          <Image
+            src="/images/form-img.png"
+            alt="form image"
+            width="35"
+            height="35"
+          />
+        </div>
         <input type="text" placeholder="Name" required />
         <input type="email" placeholder="Email" required />
         <input type="password" placeholder="Password" required />
-        <button type="submit">Sign Up</button>
+        <SecondaryButton label={"Sign In"} type="submit" />
       </form>
     </div>
   );
