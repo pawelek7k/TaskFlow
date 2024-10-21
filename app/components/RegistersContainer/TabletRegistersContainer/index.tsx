b1e880964408d16c095fea3111562dcf4e404b6a
pawelek <k.pawelek@vp.pl>
2024-10-21 15:52:06 +0200

    Update index.tsx

--- app/components/RegistersContainer/TabletRegistersContainer/index.tsx
+++ app/components/RegistersContainer/TabletRegistersContainer/index.tsx
@@ -1,3 +1,22 @@
+import { useState } from "react";
+import { PrimaryButton } from "../../Buttons/PrimaryButton";
+import { SignIn } from "../../SignIn";
+import { SignUp } from "../../SignUp";
+
 export const TabletRegistersContainer = () => {
-  return <div></div>;
+  const [isLoginPage, setIsLoginPage] = useState(true);
+
+  const togglePage = () => {
+    setIsLoginPage((prev) => !prev);
+  };
+
+  return (
+    <div>
+      {isLoginPage ? <SignIn /> : <SignUp />}{" "}
+      <PrimaryButton
+        onClick={togglePage}
+        label={isLoginPage ? "Go to Sign Up" : "Go to Sign In"}
+      />
+    </div>
+  );
 };
